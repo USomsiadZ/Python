f5842fb81c329bca5d750847b317ef35eb4e1162
Hubert Parzych <hubertparzych10@gmail.com>
2024-03-07 17:16:21 +0100

    Usuniecie zbędnych plików

--- praktyki 2023/zmiejsze projekty/kalkulator.py
+++ praktyki 2023/zmiejsze projekty/kalkulator.py
@@ -1,51 +1,76 @@
-import math
-a = str(input("Pierwsza liczba: "))
-doda = a.count("+")
-odej = a.count("-")
-mnoz = a.count("*")
-dziel = a.count("/")
-potegi = a.count("^")
-pierw = a.count("p")
-end = len(a)
-b = a.split()
-print(b)
-def doda2():
-    j = a[0:a.find("+") - 1]
-    j2 = a[a.find("+") + 2:end] 
-    print(int(j) + int(j2))
-def odej2():
-    j = a[0:a.find("-") - 1]
-    j2 = a[a.find("-") + 2:end] 
-    print(int(j) - int(j2))
-def mnoz2():
-    j = a[0:a.find("*") - 1]
-    j2 = a[a.find("*") + 2:end] 
-    print(int(j) * int(j2))
-def dziel2():
-    j = a[0:a.find("/") - 1]
-    j2 = a[a.find("/") + 2:end]
-    if int(j2)==0:
-        print("Nie mozna dzilic przez 0")
-        exit()
-    print(int(j) / int(j2))
-def potegi2():
-    j = a[0:a.find("^") - 1]
-    j2 = a[a.find("^") + 2:end] 
-    print(int(j) ** int(j2))
-def pierw2():
-    j = a[0:a.find("p") - 1]
-    j2 = a[a.find("p") + 2:end] 
-    print(math.pow(int(j), 1.0/int(j2)))
-if doda > 0:
-    doda2()
-elif odej > 0:
-    odej2()
-elif mnoz > 0:
-    mnoz2()
-elif dziel > 0:
-    dziel2()
-elif potegi > 0:
-    potegi2()
-elif pierw > 0:
-    pierw2()
+import os,time
+seconds = time.time()
+lt = time.ctime(seconds) + ": "
+
+print("Kalkulator")
+print("1 Dodawanie")
+print("2 Odejmowanie")
+print("3 Mnozenie ")
+print("4 Dzielenie")
+print("5 logi")
+def odejmowanie():
+    os.system('cls')
+    a = int(input("Pierwsza liczba: "))
+    b = int(input("Druga liczba: "))
+    c = a - b
+    
+    file1 = open("logi.txt", "a") 
+    d = lt + str(a) + " - " + str(b) + " = " + str(c)  + '\n'
+    file1.write(d)
+    file1.close()
+    return c
+def dodawanie():
+    os.system('cls')
+    a = int(input("Pierwsza liczba: "))
+    b = int(input("Druga liczba: "))
+    c = a + b
+    
+    file1 = open("logi.txt", "a") 
+    d = lt + str(a) + " + " + str(b) + " = " + str(c)  + '\n'
+    file1.write(d)
+    file1.close()
+    return c
+def mnozenie():
+    os.system('cls')
+    a = int(input("Pierwsza liczba: "))
+    b = int(input("Druga liczba: "))
+    c = a * b
+    
+    file1 = open("logi.txt", "a") 
+    d = lt + str(a) + " * " + str(b) + " = " + str(c) + '\n'
+    file1.write(d)
+    file1.close()
+    return c
+def dzielenie():
+    os.system('cls')
+    a = int(input("Pierwsza liczba: "))
+    b = int(input("Druga liczba: "))
+    if b == 0:
+        print("Nie mozna dzielic przez 0")
+    c = a / b
+    file1 = open("logi.txt", "a") 
+    d = lt + str(a) + " / " + str(b) + " = " + str(c) + '\n'
+    file1.write(d)
+    file1.close()
+    return c
+def logi():
+    os.system('cls')
+    file1 = open("logi.txt", "r")
+    print(file1.read())
+
+g = int(input())
+
+if g == 1:
+    c = dodawanie()
+elif g == 2:
+    c = odejmowanie()
+elif g == 3:
+    c = mnozenie()
+elif g == 4:
+    c = dzielenie()
+elif g == 5:
+    c = logi()
+else: print("Nie prawidlowy numer")
+if g != 5:
+    print("Wynik: ",c)
 
