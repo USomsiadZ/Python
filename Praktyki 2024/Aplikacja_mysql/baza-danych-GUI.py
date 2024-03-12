b6ce58f2121c5f99fd524f31d2e6d8612ce5eccd
USomsiadZ <hubertparzych10@gmail.com>
2024-03-12 09:57:33 +0100

    optimalize code

--- Praktyki 2024/Aplikacja_mysql/baza-danych-GUI.py
+++ Praktyki 2024/Aplikacja_mysql/baza-danych-GUI.py
@@ -1,143 +1,154 @@
 import tkinter as tk
 from tkinter import Listbox, ttk, END,messagebox
 from tkcalendar import DateEntry
-import mysql.connector
+import mysql.connector,sys
 from hashlib import sha256
 #Połączenie mysql
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="toor",
-    password="ZAQ!2wsx",
-    database="Database"
-)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="toor",
+        password="ZAQ!2wsx",
+        database="Database"
+    )
+except:
+    messagebox.showerror("Error","Nie można się połączyć z serverem")
+    sys.exit()
 cursor = mydb.cursor()
-if mydb.is_connected():
-    print("Connected to the database!")
+
 
 
 #GUI
 root = tk.Tk()
 root.geometry("800x600")
 root.title("Menu")
+email_nick = None
+
 #funkcje tworzenia gui
+class GUI_function:
+    def __init__(self,root):
+        self.root = root
+        pass
+    def forget_allwidget(self):
+        for widget in self.root.winfo_children():
+            widget.pack_forget()
+        
+    def pack_widgets(self,widgets=None,forgetallwidget=True):
+        if forgetallwidget:
+            self.forget_allwidget()
+        for widget in widgets:
+            widget.pack()
+    def create_entry(self, width, text):
+        pole = tk.Entry(self.root, width=width)
+        pole.insert(0, text)
+        pole.bind("<FocusIn>", lambda args: pole.delete('0', 'end') if pole.get() == text else None)
+        pole.bind("<FocusOut>", lambda args: pole.insert(0, text) if not pole.get() else None)
+        return pole
 class Menu:
-
+    def __init__(self):
+        self.gui = GUI_function(root)
+        pass
     def dodawania_kontaktow(self):
-        forget_allwidget()
         cofnij_button = tk.Button(root,text="cofnij",command=self.Zalogowanych)
-        pack_widgets([Nowy_kontakt_pole,Nowy_kontakt_button,cofnij_button])
+        self.gui.pack_widgets([Nowy_kontakt_pole,Nowy_kontakt_button,cofnij_button])
 
-    def głowne_pack(self):
-        forget_allwidget()
-        pack_widgets([Logowanie_butt, Menu_tworzenie_konta_butt, Menu_zapytania_button])
+    def glowne(self):
+        self.gui.pack_widgets([Logowanie_butt, Menu_tworzenie_konta_butt, Menu_zapytania_button])
 
     def login_pack(self):
-        forget_allwidget()
-        pack_widgets([email_pole_logowanie, passw_pole])
+        self.gui.pack_widgets([email_pole_logowanie, passw_pole])
 
     def zapytania(self):
-        pack_widgets([Zapytanie_A_button, Zapytanie_B_button, Zapytanie_C2_button, Zapytanie_C1_button,Zapytanie_imie_pole,Zapytanie_nazwisko_pole,Zapytanie_output])
+        self.gui.pack_widgets([Zapytanie_A_button, Zapytanie_B_button, Zapytanie_C2_button, Zapytanie_C1_button,Zapytanie_imie_pole,Zapytanie_nazwisko_pole,Zapytanie_output])
+
+        if email_nick:
+            cofnij_button = tk.Button(root,text="cofnij",command=self.Zalogowanych)
+
+        else:
+            cofnij_button = tk.Button(root,text="cofnij",command=self.glowne)
+        self.gui.pack_widgets([cofnij_button],None)
 
     def tworzenie_konta(self):
-        forget_allwidget()
-        pack_widgets([email_pole, passw_pole,r1,r2, imie_pole, Nazwisko_pole, Data_urodzenia_pole, telefon_pole, stworz_konto_button])
+        self.gui.pack_widgets([email_pole, passw_pole,r1,r2, imie_pole, Nazwisko_pole, Data_urodzenia_pole, telefon_pole, stworz_konto_button])
 
     def Zalogowanych(self):
-        forget_allwidget()
-        pack_widgets([Lista_kontaktow_button,Dodaj_kontakt_button,Lista_wpisow_button])
+        self.gui.pack_widgets([Lista_kontaktow_button,Dodaj_kontakt_button,Lista_wpisow_button,Menu_zapytania_button])
 
     def wpisy(self):
-        forget_allwidget()
         wyswietl_wpisy()
         cofnij_button = tk.Button(root, text="cofnij", command=self.Zalogowanych)
-        pack_widgets([Menu_nowy_wpis_button,Nowy_delete_wpis,cofnij_button])
+        self.gui.pack_widgets([listbox,Menu_nowy_wpis_button,Nowy_delete_wpis,cofnij_button])
         return
 
     def nowy_wpis(self):
-        forget_allwidget()
         cofnij_button = tk.Button(root, text="cofnij", command=self.wpisy)
-        pack_widgets([Nowy_wpis_pole,Nowy_dodaj_wpis,cofnij_button])
-    def zapytania_onlick():
-        forget_allwidget()
-        menu.zapytania()
+        self.gui.pack_widgets([Nowy_wpis_pole,Nowy_dodaj_wpis,cofnij_button])
+
+    def zapytania_onlick(self):
+        self.zapytania()
+class on_enter:
+    def key(event):#Enter robi to samo co myszka
+        try:
+            widget_with_focus = root.focus_get()
+            widget_with_focus.invoke()
+        except:
+            return
+    def imie_plec(event):#Autouzupełnia imie 
+        male_names = ['Adam', 'Bartek', 'Cezary', 'Dawid', 'Edward']
+        female_names = ['Anna', 'Beata', 'Celina', 'Dorota', 'Ewa']
+
+        input_text = imie_pole.get().lower()
+        if var.get() == 1:
+            for name in female_names:
+                if name.lower().startswith(input_text):
+                    imie_pole.delete(0, 'end')
+                    imie_pole.insert(0, name)
+                    break
+        elif var.get() == 2:
+            for name in male_names:
+                if name.lower().startswith(input_text):
+                    imie_pole.delete(0, 'end')
+                    imie_pole.insert(0, name)
+                    break   
+    def email(event):#Autouzupełnia email
+        #pobieranie emailów
+        query = f"SELECT email FROM `uzytkownicy`"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        input_text = email_pole_logowanie.get().lower()
+        for emails in result:
+            if emails[0].lower().startswith(input_text):
+                email_pole_logowanie.delete(0, 'end')
+                email_pole_logowanie.insert(0, emails[0])
+                break
+    def imie(event):#Autouzupełnia email
+        #pobieranie emailów
+        query = f"SELECT Imie FROM `dane_osoby`"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        input_text = Zapytanie_imie_pole.get().lower()
+        for emails in result:
+            if emails[0].lower().startswith(input_text):
+                Zapytanie_imie_pole.delete(0, 'end')
+                Zapytanie_imie_pole.insert(0, emails[0])
+                break
+    def nazwisko(event):#Autouzupełnia email
+        #pobieranie emailów
+        query = f"SELECT Nazwisko FROM `dane_osoby`"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        input_text = Zapytanie_nazwisko_pole.get().lower()
+        for emails in result:
+            if emails[0].lower().startswith(input_text):
+                Zapytanie_nazwisko_pole.delete(0, 'end')
+                Zapytanie_nazwisko_pole.insert(0, emails[0])
+                break
+    
 menu = Menu()
+gui = GUI_function(root)
 #onclick
 
 
-
-#Tworzenie nowych widgets
-def pack_widgets(widgets):
-    for widget in widgets:
-        widget.pack()
-def create_entry(root, width, text):
-    pole = tk.Entry(root, width=width)
-    pole.insert(0, text)
-    pole.bind("<FocusIn>", lambda args: pole.delete('0', 'end') if pole.get() == text else None)
-    pole.bind("<FocusOut>", lambda args: pole.insert(0, text) if not pole.get() else None)
-    return pole
-def forget_allwidget():
-    for widget in root.winfo_children():
-        widget.pack_forget()
-#On_event
-def on_enter_key(event):#Enter robi to samo co myszka
-    try:
-        widget_with_focus = root.focus_get()
-        widget_with_focus.invoke()
-    except:
-        return
-def on_enter_imie_plec(event):#Autouzupełnia imie 
-    male_names = ['Adam', 'Bartek', 'Cezary', 'Dawid', 'Edward']
-    female_names = ['Anna', 'Beata', 'Celina', 'Dorota', 'Ewa']
-
-    input_text = imie_pole.get().lower()
-    if var.get() == 1:
-        for name in female_names:
-            if name.lower().startswith(input_text):
-                imie_pole.delete(0, 'end')
-                imie_pole.insert(0, name)
-                break
-    elif var.get() == 2:
-        for name in male_names:
-            if name.lower().startswith(input_text):
-                imie_pole.delete(0, 'end')
-                imie_pole.insert(0, name)
-                break   
-def on_enter_email(event):#Autouzupełnia email
-    #pobieranie emailów
-    query = f"SELECT email FROM `uzytkownicy`"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    input_text = email_pole_logowanie.get().lower()
-    for emails in result:
-        #print(emails)
-        if emails[0].lower().startswith(input_text):
-            email_pole_logowanie.delete(0, 'end')
-            email_pole_logowanie.insert(0, emails[0])
-            break
-def on_enter_imie(event):#Autouzupełnia email
-    #pobieranie emailów
-    query = f"SELECT Imie FROM `dane_osoby`"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    input_text = Zapytanie_imie_pole.get().lower()
-    for emails in result:
-        #print(emails)
-        if emails[0].lower().startswith(input_text):
-            Zapytanie_imie_pole.delete(0, 'end')
-            Zapytanie_imie_pole.insert(0, emails[0])
-            break
-def on_enter_nazwisko(event):#Autouzupełnia email
-    #pobieranie emailów
-    query = f"SELECT Nazwisko FROM `dane_osoby`"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    input_text = Zapytanie_nazwisko_pole.get().lower()
-    for emails in result:
-        #print(emails)
-        if emails[0].lower().startswith(input_text):
-            Zapytanie_nazwisko_pole.delete(0, 'end')
-            Zapytanie_nazwisko_pole.insert(0, emails[0])
-            break
 
 #funkcje     
 def logowanie():
@@ -178,9 +189,8 @@ def stworz_konto():
         cursor.execute(query2,(email_pole.get(),hash_password,imie_pole.get(),Nazwisko_pole.get(),Data_urodzenia_pole.get(),telefon_pole.get()))
         mydb.commit()
     except:
-        print("Konto nie utworzone")
+        messagebox.showerror("Error","Konto nie mogło zostać utworzone")
     else:
-        print("Konto utworzone")
         global email_nick
         email_nick = email_pole.get()
         menu.Zalogowanych()
@@ -199,7 +209,7 @@ def wyswietl_kontakty():
         for i, row in enumerate(result):
             tree.insert("", i, values=(row[0], row[1], row[2]))
 
-    forget_allwidget()
+    gui.forget_allwidget()
 
     query = ("SELECT ku.email ,ku.imie,ku.nazwisko,ku.Data_urodzenia,ku.telefon FROM kontakty k "
              "LEFT JOIN uzytkownicy u on k.uzytkownik_ID = u.Uzytkownik_ID "
@@ -230,7 +240,6 @@ def wyswietl_kontakty():
     def on_click(event):
         item = tree.selection()[0]
         values = tree.item(item,"values")
-        print("you clicked on", values[0])
         for row in result:
             if row[0] == values[0]:
                 info = f"Data urodzenia: {row[3]}, Telefon: {row[4]}"
@@ -251,7 +260,7 @@ def wyswietl_kontakty():
 def nowykontakt():
     #Sprawdza czy dodajesz sam siebie
     if Nowy_kontakt_pole.get() == email_nick:
-        print("Nie mozesz dodac sam siebie")
+        messagebox.showerror("Error","Nie możesz dodać sam siebie")
         return
     
     q_kontakt_id,q_uzytkownika_id = f"select u.uzytkownik_ID from uzytkownicy u WHERE u.email = '{Nowy_kontakt_pole.get()}'",f"select u.uzytkownik_ID from uzytkownicy u WHERE u.email = '{email_nick}'"
@@ -266,14 +275,13 @@ def nowykontakt():
     cursor.execute(check_query)
     check_result = cursor.fetchone()
     if check_result:
-        print("Ten kontakt już istnieje")
+        messagebox.showerror("Error","Ten kontakt już istnieje")
         return
     #Wpisuje kontakt
     query = f"INSERT INTO `kontakty` (uzytkownik_ID,kontakt_ID) VALUES ({result_uzytkownik[0]},{result_kontakt[0]})"
     cursor.execute(query)
     mydb.commit()
 def nowywpis():
-    #print(uzytkownik_id, Nowy_wpis_pole.get())
     query = "INSERT INTO wpisy (Tworca_id, wpis) VALUES (%s, %s)"
     cursor.execute(query, (uzytkownik_id, Nowy_wpis_pole.get()))
     mydb.commit()
@@ -302,7 +310,6 @@ def wyswietl_wpisy():
     result = cursor.fetchall()
     global listbox
     listbox = Listbox(root,width=100)
-    listbox.pack()
     for row in result:
         listbox.insert(END, f"{row[4]}-{row[3]}:{row[0] + ' '}{row[1]},Wpis: {row[2]}")
 
@@ -366,8 +373,6 @@ def Zapytanie_C2():
     LIMIT 1
     """
     Zapytanie_C_komenda = Zapytanie_C_komenda_a + Zapytanie_C_komenda_b + Zapytanie_c_komenda_c
-    #print(imie) #Pusta wartość to Imie lub pusta wartość
-    #print(nazwisko) #Pusta wartość to Nazwisko lub pusta wartość
     cursor.execute(Zapytanie_C_komenda, imie_nazwisko)
     result = cursor.fetchall()
     
@@ -410,8 +415,6 @@ def Zapytanie_C1():
     LIMIT 1
     """
     Zapytanie_C_komenda = Zapytanie_C_komenda_a + Zapytanie_C_komenda_b + Zapytanie_c_komenda_c
-    #print(imie) #Pusta wartość to Imie lub pusta wartość
-    #print(nazwisko) #Pusta wartość to Nazwisko lub pusta wartość
     cursor.execute(Zapytanie_C_komenda, imie_nazwisko)
     result = cursor.fetchall()
     Zapytanie_output.config(state='normal')  # Włącz pole tekstowe
@@ -432,42 +435,33 @@ Lista_kontaktow_button = tk.Button(root,width=60,text="Lista kontaktow",command=
 Lista_wpisow_button = tk.Button(root,width=60,text="Lista wpisow",command=menu.wpisy)
 Dodaj_kontakt_button = tk.Button(root,width=60,text="Dodaj nowy kontakt",command=menu.dodawania_kontaktow)
 
-Nowy_kontakt_pole = create_entry(root, 60, "Kontakt")
+#Nowy kontakt
+Nowy_kontakt_pole = gui.create_entry(60, "Kontakt")
 Nowy_kontakt_button = tk.Button(root,text="Dodaj",command=nowykontakt)
 
-
-
-Nowy_wpis_pole = create_entry(root, 60, "Wpis")
+#wpisy
+Nowy_wpis_pole = gui.create_entry(60, "Wpis")
 Nowy_dodaj_wpis = tk.Button(root,text="Dodaj wpis",command=nowywpis)
 Nowy_delete_wpis = tk.Button(root, text="Usuń wpis", command=usuwanie_wpisu)
 Menu_nowy_wpis_button = tk.Button(root,text="Dodaj",command=menu.nowy_wpis)
 
-
-
-
 #Menu logowania
-email_pole_logowanie = create_entry(root, 60, "Email")
-passw_pole = create_entry(root, 60, "Hasło")
-email_pole_logowanie.bind('<Return>', on_enter_email)
+email_pole_logowanie = gui.create_entry(60, "Email")
+passw_pole = gui.create_entry(60, "Hasło")
+email_pole_logowanie.bind('<Return>', on_enter.email)
+
 #Menu tworzenia konta
-email_pole = create_entry(root, 60, "Email")
+email_pole = gui.create_entry(60, "Email")
 var = tk.IntVar(value=0)
 r1 = tk.Radiobutton(root, text='K', variable=var, value=1)
 r2 = tk.Radiobutton(root, text='M', variable=var, value=2)
-imie_pole = create_entry(root, 60, "Imie")
-imie_pole.bind('<Return>', on_enter_imie_plec)
-
-
-root.bind('<Return>', on_enter_key)
-
-
-Nazwisko_pole = create_entry(root, 60, "Nazwisko")
+Nazwisko_pole = gui.create_entry(60, "Nazwisko")
 Data_urodzenia_pole = DateEntry(root,width=57, year=2021, month=6, day=22, background='darkblue', foreground='white', borderwidth=2, date_pattern='yyyy-mm-dd')
-telefon_pole = create_entry(root, 60, "Numer telefonu")
-
-
-
+telefon_pole = gui.create_entry(60, "Numer telefonu")
 stworz_konto_button = tk.Button(root,text="Stwórz konto",command=stworz_konto)
+imie_pole = gui.create_entry(60, "Imie")
+imie_pole.bind('<Return>', on_enter.imie_plec)
+root.bind('<Return>', on_enter.key)
 
 #Menu główne
 Logowanie_butt = tk.Button(root, width=30,text="Logowanie",command=logowanie)
@@ -480,14 +474,14 @@ Zapytanie_B_button = tk.Button(root,text="Znajdź procent zatrudnionych na etaci
 Zapytanie_C2_button = tk.Button(root,text="Znajdź rodzinę najmniej zarabiającą.2 pokolenia", width=60,command=Zapytanie_C2)
 Zapytanie_C1_button = tk.Button(root,text="Znajdź rodzinę najmniej zarabiającą.1 pokolenie", width=60,command=Zapytanie_C1)
 Zapytanie_output = tk.Text(root, state='disabled')
-Zapytanie_imie_pole = create_entry(root, 60 , "Imie")
-Zapytanie_imie_pole.bind('<Return>', on_enter_imie)
-Zapytanie_nazwisko_pole = create_entry(root, 60 , "Nazwisko")
-Zapytanie_nazwisko_pole.bind('<Return>', on_enter_nazwisko)
+Zapytanie_imie_pole = gui.create_entry(60 , "Imie")
+Zapytanie_imie_pole.bind('<Return>', on_enter.imie)
+Zapytanie_nazwisko_pole = gui.create_entry(60 , "Nazwisko")
+Zapytanie_nazwisko_pole.bind('<Return>', on_enter.nazwisko)
 
 
 if __name__ == "__main__":
-    menu.głowne_pack()
+    menu.glowne()
     root.mainloop()
 
 
